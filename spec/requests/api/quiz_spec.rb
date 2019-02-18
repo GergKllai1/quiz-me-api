2d1d97b559252e1c073eeb306a2709cb67ad7915
codefactor-io <support@codefactor.io>
2019-02-18 13:06:19 +0000

    [CodeFactor] Apply fixes

--- spec/requests/api/quiz_spec.rb
+++ spec/requests/api/quiz_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Api::QuizController, type: :request do
     before do
         get '/api/quiz'
       end
-  
+
       it 'returns 200' do
         expect(response).to have_http_status(200)
       end
-  
+
       it 'should return all questions' do
         # binding.pry
         expect(JSON.parse(response.body)['data']['results'].count).to eq 6
@@ -19,17 +19,17 @@ RSpec.describe Api::QuizController, type: :request do
 
       it 'includes info category' do
         expected_category = JSON.parse(response.body)['data']['results'][0]['category']
-        expect(expected_category).to eq 'General Knowledge' 
+        expect(expected_category).to eq 'General Knowledge'
       end
 
       it 'includes info difficulty' do
         expected_difficulty = JSON.parse(response.body)['data']['results'][0]['difficulty']
-        expect(expected_difficulty).to eq 'medium' 
+        expect(expected_difficulty).to eq 'medium'
       end
 
       it 'includes info type' do
         expected_type = JSON.parse(response.body)['data']['results'][0]['type']
-        expect(expected_type).to eq 'multiple' 
+        expect(expected_type).to eq 'multiple'
       end
     end
 end
