5ea260583ab46d7af472df9eeba8f5b078dd9828
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-18 13:45:48 +0100

    Outputs quiz in json form at quiz index
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- app/services/quiz_service.rb
+++ app/services/quiz_service.rb
@@ -3,6 +3,6 @@ require 'rest-client'
 class QuizService
     def self.get_questions
         url = 'https://opentdb.com/api.php?amount=6&category=9&difficulty=medium&type=multiple'
-        data = JSON.parse(get(url))
+        data = JSON.parse(RestClient.get(url))
     end
 end
